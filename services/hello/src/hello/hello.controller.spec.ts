ed4a8e8cb4a9ba5886632c6d0f26e80b692c8f06
Marek Sladczyk <marek.sladczyk@stxnext.pl>
2020-01-28 09:23:18 +0100

    add example backend tests

--- services/hello/src/hello/hello.controller.spec.ts
+++ services/hello/src/hello/hello.controller.spec.ts
@@ -1,18 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HelloController } from './hello.controller';
+import { HelloService } from './hello.service';
+import { Hello, HelloList } from './interfaces/hello.interface';
 
 describe('Hello Controller', () => {
   let controller: HelloController;
+  let helloService: HelloService;
 
   beforeEach(async () => {
+    const helloServiceMock = {
+      findOne: jest.fn(),
+      findAll: jest.fn(),
+      addOne: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HelloController],
+      providers: [
+        {
+          provide: HelloService,
+          useValue: helloServiceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<HelloController>(HelloController);
+    helloService = module.get<HelloService>(HelloService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should find message by id', () => {
+    const message: Hello = { id: 1, message: 'test' };
+    const spy = jest.spyOn(helloService, 'findOne').mockReturnValue(message);
+
+    const result = controller.findOne({ id: 1 });
+    expect(spy).toBeCalledWith(1);
+    expect(result).toEqual(message);
+  });
+
+  it('should find all messages', () => {
+    const messages: HelloList = {
+      messages: [
+        { id: 1, message: 'test' },
+        { id: 2, message: 'test1' },
+      ],
+    };
+    const spy = jest.spyOn(helloService, 'findAll').mockReturnValue(messages);
+
+    const result = controller.findAll();
+    expect(spy).toBeCalled();
+    expect(result).toEqual(messages);
+  });
+
+  it('should add message', () => {
+    const message: Hello = { id: 1, message: 'test' };
+    const spy = jest.spyOn(helloService, 'addOne').mockReturnValue(message);
+
+    const result = controller.addOne(message);
+    expect(spy).toBeCalledWith(message);
+    expect(result).toEqual(message);
   });
 });
--- services/hello/src/hello/hello.service.spec.ts
+++ services/hello/src/hello/hello.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HelloService } from './hello.service';
+import { HelloService, mockMessages } from './hello.service';
 
 describe('HelloService', () => {
   let service: HelloService;
@@ -14,5 +14,26 @@ describe('HelloService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should find message by id', () => {
+    const result = service.findOne(1);
+
+    expect(result).toEqual(mockMessages[0]);
+  });
+
+  it('should return all messages', () => {
+    const result = service.findAll();
+
+    expect(result).toEqual({ messages: mockMessages });
+  });
+
+  it('should add message', () => {
+    const message = { id: 4, message: 'Test' };
+    const result = service.addOne(message);
+
+    expect(result).toEqual(message);
+    expect(mockMessages.length).toBe(4);
+    expect(mockMessages[3]).toEqual(message);
   });
 });
