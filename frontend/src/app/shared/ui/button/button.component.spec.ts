bc360f442bb9011f465123c195e00c4972e46db2
Marek Sladczyk <marek.sladczyk@stxnext.pl>
2020-04-24 21:38:26 +0200

    add tests to ButtonComponent

--- frontend/src/app/shared/ui/button/button.component.spec.ts
+++ frontend/src/app/shared/ui/button/button.component.spec.ts
@@ -1,10 +1,19 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  tick,
+  fakeAsync,
+} from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,10 +24,31 @@ describe('ButtonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call clicked on button click', fakeAsync(() => {
+    const button = el.query(By.css('button'));
+    const event = new MouseEvent('click');
+    spyOn(component, 'clicked');
+
+    button.triggerEventHandler('click', event);
+    tick();
+
+    expect(component.clicked).toHaveBeenCalledWith(event);
+  }));
+
+  it('should emit click event', () => {
+    spyOn(component.click, 'emit');
+    const event = new MouseEvent('click');
+
+    component.clicked(event);
+
+    expect(component.click.emit).toHaveBeenCalledWith(event);
   });
 });
